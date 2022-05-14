27e59a844ed27818966d11b2f1ec08af5e85671d
automation.pitstop <autoamtion.pitstop@gmail.com>
2022-05-14 12:48:07 +0530

    Added dataDrivenSuite

--- wdio.conf.suite.js
+++ wdio.conf.suite.js
@@ -4,9 +4,10 @@ const wdioConf = require("./wdio.conf.base.js");
 exports.config = merge(
     wdioConf.config,
     {
-        specs: ["./test/specs/**/commandsTest.js"],
+        specs: ["./test/specs/**/*.js"],
         suites: {
             parallelTest: ["./test/specs/parallel/test1.js", "./test/specs/parallel/test2.js"],
+            dataDrivenTest: ["./test/specs/loginPageTest.js", "./test/specs/csvTest.js"],
         },
         // exclude: [],
     },
